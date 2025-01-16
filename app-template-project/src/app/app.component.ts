f97944c0e6eda8b2cc2d4886766a0ab14e59a211
GengiuRobert <fncrobert3@gmail.com>
2025-01-16 22:35:19 +0200

    sidebar component created

--- app-template-project/src/app/app.component.ts
+++ app-template-project/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 
 import { NavbarComponent } from "../components/navbar/navbar.component";
 import { FooterComponent } from "../components/footer/footer.component";
+import { SidebarComponent } from "../components/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, FooterComponent],
+  imports: [NavbarComponent, FooterComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
