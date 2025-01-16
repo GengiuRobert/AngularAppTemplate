ea67e8fff0414c7e9a5dbddf3e911d59a827e5a6
GengiuRobert <fncrobert3@gmail.com>
2025-01-16 16:20:57 +0200

    set component and get component config from service

--- app-template-project/src/components/navbar/navbar.component.ts
+++ app-template-project/src/components/navbar/navbar.component.ts
@@ -15,7 +15,6 @@ export class NavbarComponent implements OnInit{
 
   ngOnInit(): void {
     const config = this.appService.getConfig();
-    console.log("Loaded config",config);
   }
 
 
--- app-template-project/src/services/appconfig.service.ts
+++ app-template-project/src/services/appconfig.service.ts
@@ -3,12 +3,23 @@ import  config  from "../assets/config.json";
 
 @Injectable({ providedIn: 'root' })
 export class AppConfigService {
-
+    
     private appConfig: any = config ;
+    private component: string = "";
 
     //get whole config
     getConfig() : any{
         return this.appConfig;
+    }
+
+    //set component to get the config
+    setComponent(componentName : "navbar" | "footer" | "sidebar" | "languageSwitcher") : void{
+        this.component = componentName;
+    }
+
+    //get config for component
+    getComponentConfig() : any{
+        return this.appConfig[this.component];
     }
 
 
