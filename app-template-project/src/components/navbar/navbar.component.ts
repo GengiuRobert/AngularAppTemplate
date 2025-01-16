94193adbb1cf7959773ea8e0c3a2c441aa34864e
GengiuRobert <fncrobert3@gmail.com>
2025-01-16 15:56:42 +0200

    testing the service

--- app-template-project/src/components/navbar/navbar.component.ts
+++ app-template-project/src/components/navbar/navbar.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppConfigService } from '../../services/appconfig.service';
 
 @Component({
+  standalone : true,
   selector: 'app-navbar',
   imports: [],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+
+export class NavbarComponent implements OnInit{
+
+  constructor(private appService: AppConfigService) { }
+
+  ngOnInit(): void {
+    const config = this.appService.getConfig();
+    console.log("Loaded config",config);
+  }
+
+
 
 }
--- app-template-project/src/services/appconfig.service.ts
+++ app-template-project/src/services/appconfig.service.ts
@@ -1,26 +1,10 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { map, Observable } from "rxjs";
+import  config  from "../assets/config.json";
 
 @Injectable({ providedIn: 'root' })
 export class AppConfigService {
 
-    private appConfig: any = null;
-    private appConfigURL: string = "assets/config.json";
-
-    constructor(private http: HttpClient) {
-
-    }
-
-    //load the whole config
-    loadConfig(): Observable<any> {
-        return this.http.get(this.appConfigURL).pipe(
-            map((dataFromJSON) => {
-                this.appConfig = dataFromJSON;
-                return dataFromJSON;
-            })
-        );
-    }
+    private appConfig: any = config ;
 
     //get whole config
     getConfig() : any{
