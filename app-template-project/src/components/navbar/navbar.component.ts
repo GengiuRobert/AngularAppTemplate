0835b5833d7f2d36baf527c281f4e56df006e1e8
GengiuRobert <fncrobert3@gmail.com>
2025-01-16 17:14:09 +0200

    id every link in config // template for navbar

--- app-template-project/src/components/navbar/navbar.component.ts
+++ app-template-project/src/components/navbar/navbar.component.ts
@@ -1,20 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { AppConfigService } from '../../services/appconfig.service';
+import { NgFor } from '@angular/common';
 
 @Component({
-  standalone : true,
+  standalone: true,
   selector: 'app-navbar',
   imports: [],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 
-export class NavbarComponent implements OnInit{
-
-  constructor(private appService: AppConfigService) { }
+export class NavbarComponent implements OnInit {
+  constructor(public appService: AppConfigService) { }
 
   ngOnInit(): void {
-    const config = this.appService.getConfig();
+    this.appService.setComponent('navbar');
   }
 
 
