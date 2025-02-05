d9682a2cdead6fab085425c314b0ec4ce20b0a21
GengiuRobert <fncrobert3@gmail.com>
2025-02-05 22:58:19 +0200

    new commit

--- app-template-project/src/app/app.config.ts
+++ app-template-project/src/app/app.config.ts
@@ -3,14 +3,14 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { HttpClientModule, provideHttpClient } from '@angular/common/http';
+import { HttpClientModule, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), 
     provideClientHydration(withEventReplay()),
-    provideHttpClient(),
-    importProvidersFrom(HttpClientModule)
+    //provideHttpClient(),
+    provideHttpClient(withInterceptorsFromDi())
   ]
 };
