02cdbd5a3b1c2fdc47cfdf336d621f2c0c7a4992
GengiuRobert <fncrobert3@gmail.com>
2025-02-09 16:56:24 +0200

    fetch countries and a display list

--- app-template-project/src/components/sidebar/sidebar.component.ts
+++ app-template-project/src/components/sidebar/sidebar.component.ts
@@ -2,14 +2,19 @@ import { Component, computed, OnInit } from '@angular/core';
 import { AppConfigService } from '../../services/appconfig.service';
 import { TranslationService } from '../../services/translation.service';
 import { CountryService } from '../../services/country.service';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [NgFor,NgIf, CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent implements OnInit {
+
+  countries: any[] = [];
+  loading = false;
+  error = '';
 
   constructor(public appService: AppConfigService,
     public translateService: TranslationService,
@@ -23,12 +28,19 @@ export class SidebarComponent implements OnInit {
   sidebarLinks = computed(() => this.appService.getSidebarLinks());
 
   fetchCountries(): void {
+    this.loading = true;
+    this.error = '';
+    console.log('Fetching countries...');
     this.countryService.getCountries().subscribe({
       next: (data) => {
-        console.log('Countries fetched:');
+        console.log('Countries fetched:', data);
+        this.countries = data;
+        this.loading = false;
       },
       error: (err) => {
-        console.error('Error:');
+        console.error('Error:', err);
+        this.error = 'Failed to load countries';
+        this.loading = false;
       }
     });
   }
