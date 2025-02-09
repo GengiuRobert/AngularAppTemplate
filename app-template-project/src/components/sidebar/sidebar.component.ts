21ce8a1fbd32d6f853cc88c586af392d8c48815a
GengiuRobert <fncrobert3@gmail.com>
2025-02-09 17:59:34 +0200

    version with loading spinner working

--- app-template-project/src/components/sidebar/sidebar.component.ts
+++ app-template-project/src/components/sidebar/sidebar.component.ts
@@ -27,16 +27,31 @@ export class SidebarComponent implements OnInit {
 
   sidebarLinks = computed(() => this.appService.getSidebarLinks());
 
-
   fetchCountries(): void {
     this.countries = [];
     this.loading = true;
     this.error = '';
 
+    const cachedData = localStorage.getItem('countriesData');
+    const cacheTime = localStorage.getItem('countriesCacheTime');
+    const now = new Date().getTime();
+
+    if (cachedData && cacheTime && (now - parseInt(cacheTime, 10)) < 24 * 60 * 60 * 1000) {
+      console.log('Loading countries from cache...');
+      setTimeout(() => {
+        this.countries = JSON.parse(cachedData);
+        this.loading = false;
+      }, 1500);
+      return;
+    }
+
+    console.log('Fetching countries...');
     this.countryService.getCountries().subscribe({
       next: (data) => {
         console.log('Countries fetched:', data);
         this.countries = data;
+        localStorage.setItem('countriesData', JSON.stringify(data));
+        localStorage.setItem('countriesCacheTime', now.toString());
         this.loading = false;
       },
       error: (err) => {
@@ -45,5 +60,5 @@ export class SidebarComponent implements OnInit {
         this.loading = false;
       }
     });
-}
+  }
 }
